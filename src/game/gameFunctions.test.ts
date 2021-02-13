54aef36b6c3b7f2a4ab535cb2f771cf72d100ac6
nbogie <neillbogie@googlemail.com>
2021-02-13 00:15:59 +0000

    correct expected value in test

--- src/game/gameFunctions.test.ts
+++ src/game/gameFunctions.test.ts
@@ -11,5 +11,5 @@ test("scoreCards", () => {
   expect(scoreCards([7])).toBe(7);
   expect(scoreCards([10, 11])).toBe(10);
   expect(scoreCards([10, 20, 30])).toBe(10 + 20 + 30);
-  expect(scoreCards([3, 10, 11, 12, 17, 18, 19])).toBe(3 + 11 + 17);
+  expect(scoreCards([3, 10, 11, 12, 17, 18, 19])).toBe(3 + 10 + 17);
 });

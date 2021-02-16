f9fdca9175ec47b4b3efc126747cf2dc1d0b854d
nbogie <neillbogie@googlemail.com>
2021-02-16 01:17:34 +0000

    simplify getFullDeck()

--- src/game/deckFunctions.ts
+++ src/game/deckFunctions.ts
@@ -7,9 +7,7 @@ const shuffleDeck = (deck: Card[]) => {
 };
 
 const getFullDeck = (): Card[] => {
-  const initialArray = [...Array(33).keys()];
-  const correctedNumbers = initialArray.map((number) => number + 3);
-  return correctedNumbers as Card[];
+  return [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 };
 
 const makeNewDeck = (): Card[] => {
